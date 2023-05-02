1941f118497683c1194ce671c18efc786a1fbc3d
Jannik Kastens <jannik.kastens@hmmh.de>
2023-05-02 11:39:16 +0200

    Added person doc

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,26 +1,50 @@
 package model;
 
+/**
+ * Person is the parent class for patients and nurses.
+ */
 public abstract class Person {
     private String firstName;
     private String surname;
 
+    /**
+     * provides constructor for child-classes
+     * @param firstName
+     * @param surname
+     */
     public Person(String firstName, String surname) {
         this.firstName = firstName;
         this.surname = surname;
     }
 
+    /**
+     *
+     * @return person/object first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     *
+     * @param firstName gets set as new first name for object
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     *
+     * @return person/object surname
+     */
     public String getSurname() {
         return surname;
     }
 
+    /**
+     *
+     * @param surname gets set as new surname for object
+     */
     public void setSurname(String surname) {
         this.surname = surname;
     }
